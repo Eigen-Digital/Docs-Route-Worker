3662a99c389503658dfa18fe6b5450d5d311aad6
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2026-09-23 13:00:46 +0000

    Update fetch logic for '/sheetx/' requests

--- worker.js
+++ worker.js
@@ -3,7 +3,8 @@ export default {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith('/sheetx/')) {
-      return fetch(new Request('https://sheetx.pages.dev/', request));
+      const path = url.pathname.slice(8);
+      return fetch(new Request(`https://sheetx.pages.dev/${path}`, request));
     }
 
     return new Response(
