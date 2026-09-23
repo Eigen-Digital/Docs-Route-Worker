3e72e3056554eecdc976020e64466c8387d4323c
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2026-09-23 11:48:47 +0000

    Implement fetch handler in worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,15 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
 
+    return new Response(
+      `Docs router is working.\nPath: ${url.pathname}`,
+      {
+        status: 200,
+        headers: {
+          "content-type": "text/plain",
+        },
+      }
+    );
+  },
+};
